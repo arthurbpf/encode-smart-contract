9125f6ef594e0ac4b4150a7fc90f99a811954c3d
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-03-21 09:38:00 -0300

    style: code formatting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config'
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.0",
-  networks: {
-    goerli: {
-      url: process.env.ALCHEMY_GOERLI_URL,
-      accounts:[process.env.GOERLI_WALLET_PRIVATE_KEY!]
-    }
-  }
+	solidity: '0.8.0',
+	networks: {
+		goerli: {
+			url: process.env.ALCHEMY_GOERLI_URL,
+			accounts: [process.env.GOERLI_WALLET_PRIVATE_KEY!]
+		}
+	}
 };
 
 export default config;
--- scripts/run.js
+++ scripts/run.js
@@ -1,33 +1,33 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
-  await waveContract.deployed();
+	const [owner, randomPerson] = await hre.ethers.getSigners();
+	const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+	const waveContract = await waveContractFactory.deploy();
+	await waveContract.deployed();
 
-  console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
+	console.log('Contract deployed to:', waveContract.address);
+	console.log('Contract deployed by:', owner.address);
 
-  let waveCount = await waveContract.getTotalWaves();
+	let waveCount = await waveContract.getTotalWaves();
 
-  let waveTxn = await waveContract.wave();
-  await waveTxn.wait();
+	let waveTxn = await waveContract.wave();
+	await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+	waveCount = await waveContract.getTotalWaves();
 
-  waveTxn = await waveContract.connect(randomPerson).wave();
-  await waveTxn.wait();
+	waveTxn = await waveContract.connect(randomPerson).wave();
+	await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+	waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 runMain();
