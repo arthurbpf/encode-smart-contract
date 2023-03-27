5252f27b12cb5671175f791247137f8c0e53e06b
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-03-26 23:44:56 -0300

    feat: start buying and selling logic implementation

--- contracts/Encode.sol
+++ contracts/Encode.sol
@@ -6,16 +6,41 @@ import 'hardhat/console.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 
-contract Encode is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Encode is ERC721, ERC721URIStorage, ERC721Burnable {
+	using Counters for Counters.Counter;
+	Counters.Counter private _tokenIdCounter;
+
+	enum BuyingRequestStatus {
+		PENDING,
+		ACCEPTED,
+		REJECTED
+	}
+
+	struct BuyingRequest {
+		address buyer;
+		uint256 offer;
+		uint256 timestamp;
+		BuyingRequestStatus status;
+	}
+
+	// maps token id to buying requests
+	mapping(uint256 => BuyingRequest[]) public buyingRequests;
+
+	struct SellingListing {
+		uint256 price;
+		uint256 timestamp;
+	}
+
+	// maps address to selling listings
+	mapping(uint256 => SellingListing) public sellingListings;
+
 	constructor() ERC721('Encode', 'ENC') {}
 
-	function safeMint(
-		address to,
-		uint256 tokenId,
-		string memory uri
-	) public onlyOwner {
+	function safeMint(address to, string memory uri) public {
+		uint256 tokenId = _tokenIdCounter.current();
+		_tokenIdCounter.increment();
 		_safeMint(to, tokenId);
 		_setTokenURI(tokenId, uri);
 	}
@@ -30,5 +55,42 @@ contract Encode is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 		uint256 tokenId
 	) public view override(ERC721, ERC721URIStorage) returns (string memory) {
 		return super.tokenURI(tokenId);
+	}
+
+	function createBuyingRequest(uint256 tokenId, uint256 offer) public {
+		require(ownerOf(tokenId) != msg.sender, 'You cannot buy your own NFT');
+		require(offer > 0, 'Offer must be greater than 0');
+
+		buyingRequests[tokenId].push(
+			BuyingRequest(
+				msg.sender,
+				offer,
+				block.timestamp,
+				BuyingRequestStatus.PENDING
+			)
+		);
+	}
+
+	function getBuyingRequests(
+		uint256 tokenId
+	) public view returns (BuyingRequest[] memory) {
+		return buyingRequests[tokenId];
+	}
+
+	function createSellingListing(uint256 tokenId, uint256 price) public {
+		require(
+			ownerOf(tokenId) == msg.sender,
+			'You cannot sell NFTs that you do not own'
+		);
+		require(price > 0, 'Price must be greater than 0');
+
+		approve(address(this), tokenId);
+		sellingListings[tokenId] = SellingListing(price, block.timestamp);
+	}
+
+	function getSellingListing(
+		uint256 tokenId
+	) public view returns (SellingListing memory) {
+		return sellingListings[tokenId];
 	}
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,56 @@
 const main = async () => {
 	const [owner, randomPerson] = await hre.ethers.getSigners();
-	const waveContractFactory = await hre.ethers.getContractFactory('Encode');
-	const waveContract = await waveContractFactory.deploy();
-	await waveContract.deployed();
+	const encodeContractFactory = await hre.ethers.getContractFactory('Encode');
+	const encodeContract = await encodeContractFactory.deploy();
+	await encodeContract.deployed();
 
-	console.log('Contract deployed to:', waveContract.address);
+	console.log('Contract deployed to:', encodeContract.address);
 	console.log('Contract deployed by:', owner.address);
 
-	/*
+	let balance = await encodeContract.balanceOf(randomPerson.address);
+	console.log('Balance of randomPerson:', balance.toString());
 
+	let mintTxn = await encodeContract
+		.connect(randomPerson)
+		.safeMint(randomPerson.address, 'https://test');
+	await mintTxn.wait();
+
+	balance = await encodeContract.balanceOf(randomPerson.address);
+	console.log('Balance of randomPerson after minting:', balance.toString());
+
+	let tokenURI = await encodeContract.tokenURI(0);
+	console.log('Token URI: ', tokenURI);
+
+	try {
+		let illegalTransferTxn = await encodeContract[
+			'safeTransferFrom(address,address,uint256)'
+		](randomPerson.address, owner.address, 0);
+		await illegalTransferTxn.wait();
+	} catch (error) {
+		console.error('Error: ', "You don't own this token");
+	}
+
+	try {
+		let legalTransferTxn = await encodeContract
+			.connect(randomPerson)
+			['safeTransferFrom(address,address,uint256)'](
+				randomPerson.address,
+				owner.address,
+				0
+			);
+
+		await legalTransferTxn.wait();
+	} catch (error) {
+		console.error('Error: ', error);
+	}
+
+	balance = await encodeContract.balanceOf(randomPerson.address);
+	console.log('Balance of randomPerson after transfer:', balance.toString());
+
+	balance = await encodeContract.balanceOf(owner.address);
+	console.log('Balance of owner after transfer:', balance.toString());
+
+	/*
 	let contractBalance = await hre.ethers.provider.getBalance(
 		waveContract.address
 	);
