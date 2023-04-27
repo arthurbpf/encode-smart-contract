9599703b9e8c36e9525a5433f2d770a7330f5f6a
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-04-27 08:25:33 -0300

    test: improve test run to encompass new features

--- scripts/run.js
+++ scripts/run.js
@@ -8,15 +8,18 @@ const main = async () => {
 	console.log('Contract deployed by:', owner.address);
 
 	let balance = await encodeContract.balanceOf(randomPerson.address);
-	console.log('Balance of randomPerson:', balance.toString());
+	console.log('Balance of owner:', balance.toString());
 
-	let mintTxn = await encodeContract
-		.connect(randomPerson)
-		.safeMint(randomPerson.address, 'https://test');
+	let mintTxn = await encodeContract.safeMint(
+		owner.address,
+		'https://test',
+		'title',
+		'description'
+	);
 	await mintTxn.wait();
 
-	balance = await encodeContract.balanceOf(randomPerson.address);
-	console.log('Balance of randomPerson after minting:', balance.toString());
+	balance = await encodeContract.balanceOf(owner.address);
+	console.log('Balance of owner after minting:', balance.toString());
 
 	let tokenURI = await encodeContract.tokenURI(0);
 	console.log('Token URI: ', tokenURI);
@@ -50,47 +53,34 @@ const main = async () => {
 	balance = await encodeContract.balanceOf(owner.address);
 	console.log('Balance of owner after transfer:', balance.toString());
 
-	let tokens = await encodeContract.getTokensOfOwner(owner.address);
-	console.log(owner.address);
-	console.log(tokens);
-
-	/*
-	let contractBalance = await hre.ethers.provider.getBalance(
-		waveContract.address
+	let randomPersonBalance = await hre.ethers.provider.getBalance(
+		randomPerson.address
 	);
-
 	console.log(
-		'Contract balance:',
-		hre.ethers.utils.formatEther(contractBalance)
+		'Balance of randomPerson before placing requests:',
+		randomPersonBalance.toString()
 	);
 
-	let waveTxn = await waveContract.wave('Primeira mensagem!');
-	await waveTxn.wait();
+	let buyingRequestTxn = await encodeContract
+		.connect(randomPerson)
+		.createBuyingRequest(0, 100);
+	await buyingRequestTxn.wait();
 
-	contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+	let buyingRequestTxn2 = await encodeContract
+		.connect(randomPerson)
+		.createBuyingRequest(0, 1000);
+	await buyingRequestTxn2.wait();
 
+	randomPersonBalance = await hre.ethers.provider.getBalance(
+		randomPerson.address
+	);
 	console.log(
-		'Contract balance:',
-		hre.ethers.utils.formatEther(contractBalance)
+		'Balance of randomPerson after placing requests:',
+		randomPersonBalance.toString()
 	);
 
-	waveTxn = await waveContract.connect(randomPerson).wave('Outra mensagem!');
-	await waveTxn.wait();
-
-	contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
-
-	console.log(
-		'Contract balance:',
-		hre.ethers.utils.formatEther(contractBalance)
-	);
-
-	let allWaves = await waveContract.getAllWaves();
-	console.log(allWaves);
-
-	// Should not complete given cooldown strategy
-	waveTxn = await waveContract.connect(randomPerson).wave('Outra mensagem!');
-	await waveTxn.wait();
-	*/
+	let getBuyingRequests = await encodeContract.getBuyingRequests(0);
+	console.log(getBuyingRequests);
 };
 
 const runMain = async () => {
